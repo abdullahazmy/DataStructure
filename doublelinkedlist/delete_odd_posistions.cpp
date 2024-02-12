bac354361d89a6d182bfece4f7e561d1462133a6
Abdullah Azmy <125517573+abdullahazmy@users.noreply.github.com>
2024-02-13 01:15:25 +0200

    Update delete_odd_posistions.cpp

--- doublelinkedlist/delete_odd_posistions.cpp
+++ doublelinkedlist/delete_odd_posistions.cpp
@@ -1,597 +1,271 @@
-#include <bits/stdc++.h>
-#define ll long long
+#include <iostream>
 using namespace std;
-void fast()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-}
-class Vector
-{
-private:
-    int *arr{nullptr};
-    int size{0};    // user size
-    int capacity{}; // actual size
-    void expand_capacity()
-    {
-        // Double the actual array size
-        capacity *= 2;
-        cout << "Expand capacity to "
-             << capacity << "\n";
-        int *arr2 = new int[capacity]{};
-        for (int i = 0; i < size; ++i)
-            arr2[i] = arr[i]; // copy data
-
-        swap(arr, arr2);
-        delete[] arr2;
-    }
-
-public:
-    Vector(int size) : size(size)
-    {
-        if (size < 0)
-            size = 1;
-        capacity = size + 10;
-        arr = new int[capacity]{};
-    }
-    ~Vector()
-    {
-        delete[] arr;
-        arr = nullptr;
-    }
-    int get_size()
-    {
-        return size;
-    }
-    int get(int idx)
-    {
-        assert(0 <= idx && idx < size);
-        return arr[idx];
-    }
-    void set(int idx, int val)
-    {
-        assert(0 <= idx && idx < size);
-        arr[idx] = val;
-    }
-    void print()
-    {
-        for (int i = 0; i < size; ++i)
-            cout << arr[i] << " ";
-        cout << "\n";
-    }
-    int find(int value)
-    {
-        for (int i = 0; i < size; ++i)
-            if (arr[i] == value)
-                return i;
-        return -1; // -1 for NOT found
-    }
-    int get_front()
-    {
-        return arr[0];
-    }
-    int get_back()
-    {
-        return arr[size - 1];
-    }
-    void push_back(int value)
-    {
-        if (size == capacity)
-            expand_capacity();
-        arr[size++] = value;
-    }
-    void insert(int idx, int value)
-    {
-        assert(0 <= idx && idx < size);
-
-        if (size == capacity)
-            expand_capacity();
-
-        // Shift all the data to right first
-        for (int p = size - 1; p >= idx; --p)
-            arr[p + 1] = arr[p];
-
-        arr[idx] = value;
-        ++size;
-    }
-    void right_rotate()
-    {
-        if (size <= 1)
-            return;
-        int end = arr[size - 1];
-        for (int i = size - 1; i >= 1; --i) // 0 1 2 3 4
-            arr[i] = arr[i - 1];            // 4 0 1 2 3
-        arr[0] = end;
-    }
-    void right_rotate(int times)
-    {
-        if (size <= 1)
-            return;
-        times = times % size;
-        while (times--)
-        {
-            right_rotate();
-        }
-    }
-    void left_rotate()
-    {
-        if (size <= 1)
-            return;
-        int start = arr[0];
-        for (int i = 0; i < size - 1; ++i) // 0 1 2 3 4
-            arr[i] = arr[i + 1];           // 1 2 3 4 0
-        arr[size - 1] = start;
-    }
-    void left_rotate(int times)
-    {
-        if (size <= 1)
-            return;
-        times = times % size;
-        while (times--)
-        {
-            left_rotate();
-        }
-    }
-    int pop(int idx)
-    {
-        assert(0 <= idx && idx < size);
-        int val = arr[idx];
-        for (int i = idx; i < size - 1; i++)
-            arr[i] = arr[i + 1];
-        --size;
-        return val;
-    }
-    int find_transposition(int value)
-    {
-        int idx = find(value);
-        if (idx == -1)
-            return -1;
-        if (idx == 0)
-            return 0;
-        swap(arr[idx], arr[idx - 1]);
-        return idx - 1;
-    }
-};
 struct Node
 {
-    int data{};
+    int data;
     Node *next{};
-    Node *prev{}; // Previous node!
-
+    Node *prv{};
     Node(int data) : data(data) {}
-
-    void set(Node *next, Node *prev)
-    {
-        this->next = next;
-        this->prev = prev;
-    }
-
-    ~Node()
-    {
-        cout << "Destroy value: " << data << " at address " << this << "\n";
-    }
 };
 
-class LinkedList
+class doubleLinkedList
 {
 private:
     Node *head{};
     Node *tail{};
-    int length = 0;
-
-    // let's maintain how many nodes
-
-    vector<Node *> debug_data; // add/remove nodes you use
-
-    void debug_add_node(Node *node)
-    {
-        debug_data.push_back(node);
-    }
-    void debug_remove_node(Node *node)
-    {
-        auto it = std::find(debug_data.begin(), debug_data.end(), node);
-        if (it == debug_data.end())
-            cout << "Node does not exist\n";
-        else
-            debug_data.erase(it);
-    }
+    int length{};
+    void link(Node *first, Node *second);
+    void add_node(Node *node);
+    void delete_node(Node *cur);
+    Node *delete_and_link(Node *cur);
 
 public:
-    // Below 2 deletes prevent copy and assign to avoid this mistake
-    LinkedList() {}
-    LinkedList(const LinkedList &) = delete;
-    LinkedList &operator=(const LinkedList &another) = delete;
+    void insert_end(int val);
+    void insert_front(int val);
+    void insert_sorted(int val); // By Value
+    void delete_front();
+    void delete_end();
+    void delete_node_with_key(int key);
+    void delete_all_nodes_with_key(int k);
+    void delete_even_positions();
+    void delete_odd_positions();
+    void print();
+};
 
-    void debug_print_address()
-    {
-        for (Node *cur = head; cur; cur = cur->next)
-            cout << cur << "," << cur->data << "\t";
-        cout << "\n";
-    }
-    void debug_print_node(Node *node, bool is_seperate = false)
-    {
-        if (is_seperate)
-            cout << "Sep: ";
-        if (node == nullptr)
-        {
-            cout << "nullptr\n";
-            return;
-        }
+void doubleLinkedList::link(Node *first, Node *second)
+{
+    if (first)
+        first->next = second;
+    if (second)
+        second->prv = first;
+}
 
-        if (node->prev == nullptr)
-            cout << "X\t";
-        else
-            cout << node->prev->data << "\t";
+////////////////////////////////////////////
 
-        cout << " <= [" << node->data << "]\t => \t";
+void doubleLinkedList::add_node(Node *node)
+{
+    if (!head)
+    {
+        head = tail = node;
+    }
+    link(tail, node);
+    tail = node;
+    tail->next = nullptr;
+    length++;
+}
 
-        if (node->next == nullptr)
-            cout << "X\t";
-        else
-            cout << node->next->data << "\t";
+void doubleLinkedList::delete_node(Node *cur)
+{
+    if (!head)
+        return;
+    delete (cur);
+    length--;
+}
 
-        if (node == head)
-            cout << "head\n";
-        else if (node == tail)
-            cout << "tail\n";
-        else
-            cout << "\n";
-    }
-    void debug_print_list(string msg = "")
-    {
-        if (msg != "")
-            cout << msg << "\n";
-        for (int i = 0; i < (int)debug_data.size(); ++i)
-            debug_print_node(debug_data[i]);
-        cout << "************\n"
-             << flush;
-    }
-    string debug_to_string()
-    {
-        if (length == 0)
-            return "";
-        ostringstream oss;
-        for (Node *cur = head; cur; cur = cur->next)
-        {
-            oss << cur->data;
-            if (cur->next)
-                oss << " ";
-        }
-        return oss.str();
-    }
-    void debug_verify_data_integrity()
-    {
-        if (length == 0)
-        {
-            assert(head == nullptr);
-            assert(tail == nullptr);
-        }
-        else
-        {
-            assert(head != nullptr);
-            assert(tail != nullptr);
-            if (length == 1)
-                assert(head == tail);
-            else
-                assert(head != tail);
-            assert(!head->prev);
-            assert(!tail->next);
-        }
-        int len = 0;
-        for (Node *cur = head; cur; cur = cur->next, len++)
-        {
-            if (len == length - 1) // make sure we end at tail
-                assert(cur == tail);
-        }
+/////////////////////////////////////////////
 
-        assert(length == len);
-        assert(length == (int)debug_data.size());
-
-        len = 0;
-        for (Node *cur = tail; cur; cur = cur->prev, len++)
-        {
-            if (len == length - 1) // make sure we end at head
-                assert(cur == head);
-        }
-        cout << "\n";
-    }
-    ////////////////////////////////////////////////////////////
-    // These 2 simple functions just to not forget changing the vector and length
-    void delete_node(Node *node)
+Node *doubleLinkedList::delete_and_link(Node *cur)
+{
+    if (!head || !length)
+        return nullptr;
+    Node *prv = cur->prv;
+    if (length <= 1)
     {
-        debug_remove_node(node);
-        --length;
-        delete node;
-    }
-    void add_node(Node *node)
-    {
-        debug_add_node(node);
-        ++length;
-    }
-    void print()
-    {
-        for (Node *cur = head; cur; cur = cur->next)
-            cout << cur->data << " ";
-        cout << "\n";
-    }
-    void print_reversed()
-    {
-        for (Node *cur = tail; cur; cur = cur->prev)
-            cout << cur->data << " ";
-        cout << "\n";
+        delete_front();
+        return nullptr;
     }
 
-    void link(Node *first, Node *second)
+    bool is_tail = (tail == cur);
+    if (is_tail)
     {
-        if (first)
-            first->next = second;
-        if (second)
-            second->prev = first;
-    }
-    void insert_end(int value)
-    {
-        Node *item = new Node(value);
-        add_node(item);
-
-        if (!head)
-        {
-            head = tail = item;
-        }
-        else
-        {
-            link(tail, item);
-            tail = item;
-        }
-        debug_verify_data_integrity();
-    }
-    void insert_front(int value)
-    {
-        Node *item = new Node(value);
-        add_node(item);
-
-        if (!head)
-            head = tail = item;
-        else
-        {
-            link(item, head);
-            head = item;
-        }
-        debug_verify_data_integrity();
-    }
-    void insert_sorted(int value)
-    {
-        if (!length || value <= head->data)
-            insert_front(value);
-        else if (value >= tail->data)
-            insert_end(value);
-        else
-        {
-            for (Node *cur = head; cur; cur = cur->next)
-            {
-                if (value <= cur->data)
-                {
-                    embed_after(cur->prev, value);
-                    break;
-                }
-            }
-        }
-        debug_verify_data_integrity();
-    }
-    void embed_after(Node *node_before, int value)
-    { // Add a node with value between node and its next
-        Node *middle = new Node(value);
-        add_node(middle);
-        Node *node_after = node_before->next;
-        link(node_before, middle);
-        link(middle, node_after);
-    }
-
-    void delete_front()
-    {
-        if (!length)
-            return;
-        else if (length == 1)
-        {
-            delete_node(head);
-            head = tail = nullptr;
-        }
-        else
-        {
-            head = head->next;
-            delete_node(head->prev);
-            head->prev = nullptr;
-        }
-        debug_verify_data_integrity();
-    }
-    void delete_end()
-    {
-        if (!length)
-            return;
-        else if (length == 1)
-        {
-            delete_node(tail);
-            head = tail = nullptr;
-        }
-        else
-        {
-            tail = tail->prev;
-            delete_node(tail->next);
-            tail->next = nullptr;
-        }
-        debug_verify_data_integrity();
-    }
-    Node *delete_and_link(Node *cur)
-    {
-        Node *prv = cur->prev;
-        link(cur->prev, cur->next);
-        delete_node(cur);
+        delete_end();
         return prv;
     }
+    link(cur->prv, cur->next);
+    delete_node(cur);
+    return prv;
+}
 
-    void delete_node_with_key(int value)
+//////////////////////////////////////////////
+
+void doubleLinkedList::insert_end(int val)
+{
+    Node *item = new Node(val);
+    if (!head)
     {
-        if (!length)
+        head = tail = item;
+        length++;
+        return;
+    }
+
+    add_node(item);
+}
+
+/////////////////////////////////////////////
+
+void doubleLinkedList::insert_front(int val)
+{
+    Node *item = new Node(val);
+    if (!head)
+    {
+        head = tail = item;
+        length++;
+        return;
+    }
+
+    length++;
+    link(item, head);
+    head = item;
+}
+
+////////////////////////////////////////////////
+
+void doubleLinkedList::delete_front()
+{
+    if (!head)
+        return;
+    Node *tmp = head;
+    if (length <= 1)
+    {
+        delete_node(tmp);
+        head = tail = nullptr;
+        return;
+    }
+
+    head = head->next;
+    delete_node(tmp);
+}
+
+/////////////////////////////////////////////////
+
+void doubleLinkedList::delete_end()
+{
+    if (!head)
+        return;
+    Node *tmp = tail;
+    if (length <= 1)
+    {
+        delete_node(tmp);
+        head = tail = nullptr;
+        return;
+    }
+    tail = tail->prv;
+    delete_node(tmp);
+    tail->next = nullptr;
+}
+
+///////////////////////////////////////////
+
+void doubleLinkedList::delete_node_with_key(int key)
+{
+    if (!head)
+        return;
+
+    // If the key is found at the head, delete the head node
+    if (head->data == key)
+    {
+        delete_front();
+        return;
+    }
+
+    // If the key is found at the tail, delete the tail node
+
+    // Traverse the list to find the node with the given key
+    for (Node *cur = head; cur; cur = cur->next)
+    {
+        if (cur->data == key)
+        {
+            // Link the previous node to the next node, skipping the current node
+            cur->prv->next = cur->next;
+            cur->next->prv = cur->prv;
+
+            if (cur->data == tail->data == key)
+            {
+                delete_end();
+                return;
+            }
+            // Delete the current node
+            delete_node(cur);
             return;
-        if (head->data == value)
-            delete_front();
+        }
+    }
+}
+
+void doubleLinkedList::delete_all_nodes_with_key(int k)
+{
+    if (!head || !length)
+        return;
+
+    for (Node *cur = head; cur;)
+    {
+        if (cur->data == k)
+        {
+            if (length <= 1)
+            {
+                if (cur->data == k)
+                    delete_front();
+                return;
+            }
+
+            if (cur == head)
+            {
+                cur = cur->next;
+                delete_front();
+                continue;
+            }
+
+            cur = delete_and_link(cur);
+        }
+
         else
         {
-            for (Node *cur = head; cur; cur = cur->next)
-            {
-                if (value == cur->data)
-                {
-                    cur = delete_and_link(cur);
-                    if (!cur->next) // we removed last node!
-                        tail = cur;
-                }
-            }
+            cur = cur->next;
         }
-        debug_verify_data_integrity();
     }
+    head->prv = nullptr;
+    tail->next = nullptr;
+}
 
-    void delete_all_nodes_with_key(int value)
-    {
-        if (!length)
-            return;
-        insert_front(value - 1); // dummy
-        for (Node *cur = head->next; cur;)
-        {
-            if (cur->data == value)
-            {
-                cur = delete_and_link(cur);
-                if (!cur->next)
-                    tail = cur;
-            }
-            else
-                cur = cur->next;
-        }
-        delete_front();
-
-        debug_verify_data_integrity();
-    }
-
-    void delete_even_positions()
+void doubleLinkedList::delete_even_positions()
+{
+    int ctr = 1;
+    for (Node *cur = head; cur; cur = cur->next)
     {
         if (length <= 1)
             return;
-        // cur is always odd and ->next is even
-        for (Node *cur = head; cur && cur->next; cur = cur->next)
+        if (ctr % 2 == 0)
         {
-            delete_and_link(cur->next);
-            if (!cur->next)
-                tail = cur; // in even length, the tail will change
+            cur = delete_and_link(cur);
         }
-        debug_verify_data_integrity();
+        ctr++;
     }
-    void delete_odd_positions()
-    {
-        // let's make odd positions even! Reuse old code :)
-        insert_front(-1); // dummy node
-        delete_even_positions();
-        delete_front();
-    }
-};
-void test0()
-{
-    cout << "\n\ntest0\n";
-    LinkedList list;
-
-    list.insert_end(1);
-    list.insert_end(2);
-    list.insert_end(3);
-    list.delete_odd_positions();
-    // some actions
-    list.print();
-
-    string expected = "2";
-    string result = list.debug_to_string();
-    if (expected != result)
-    {
-        cout << "no match:\nExpected: " << expected << "\nResult  : " << result << "\n";
-        assert(false);
-    }
-    list.debug_print_list("********");
 }
 
-void test1()
+void doubleLinkedList::delete_odd_positions()
 {
-    cout << "\n\ntest1\n";
-    LinkedList list;
-
-    list.insert_end(1);
-    list.insert_end(2);
-    list.insert_end(3);
-    list.insert_end(4);
-    list.insert_end(5);
-    list.delete_odd_positions();
-    // some actions
-    list.print();
-
-    string expected = "2 4";
-    string result = list.debug_to_string();
-    if (expected != result)
-    {
-        cout << "no match:\nExpected: " << expected << "\nResult  : " << result << "\n";
-        assert(false);
-    }
-    list.debug_print_list("********");
+    insert_front(-1); // dummy node
+    delete_even_positions();
+    delete_front();
 }
 
-void test2()
+void doubleLinkedList::print()
 {
-    cout << "\n\ntest2\n";
-    LinkedList list;
-
-    list.insert_end(1);
-    list.insert_end(2);
-    list.insert_end(3);
-    list.insert_end(4);
-    list.insert_end(5);
-    list.insert_end(6);
-    list.delete_odd_positions();
-    // some actions
-    list.print();
-
-    string expected = "2 4 6";
-    string result = list.debug_to_string();
-    if (expected != result)
+    for (Node *cur = head; cur; cur = cur->next)
     {
-        cout << "no match:\nExpected: " << expected << "\nResult  : " << result << "\n";
-        assert(false);
+        cout << cur->data << " ";
     }
-    list.debug_print_list("********");
+    cout << "\n";
 }
 
-void test3()
-{
-    cout << "\n\ntest3\n";
-    LinkedList list;
-
-    list.insert_end(1);
-    list.insert_end(2);
-    list.delete_odd_positions();
-    list.print();
-
-    string expected = "2";
-    string result = list.debug_to_string();
-    if (expected != result)
-    {
-        cout << "no match:\nExpected: " << expected << "\nResult  : " << result << "\n";
-        assert(false);
-    }
-    list.debug_print_list("********");
-}
-
+//////////////////////////////////////////
 int main()
 {
-    test0();
-    test1();
-    test2();
-    test3();
-
-    // must see it, otherwise RTE
-    cout << "\n\nNO RTE\n";
-
-    return 0;
+    doubleLinkedList ll;
+    ll.insert_end(1);
+    ll.insert_end(2);
+    ll.insert_end(3);
+    ll.insert_end(4);
+    ll.delete_odd_positions();
+    ll.print();
+    cout << "azmy";
 }
