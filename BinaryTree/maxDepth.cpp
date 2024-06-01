3615c23d8e54666bc14cd824fa276eddea0350d2
Abdullah Azmy <125517573+abdullahazmy@users.noreply.github.com>
2024-06-01 07:14:46 +0300

    Create maxDepth.cpp

---
+++ BinaryTree/maxDepth.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root)
+            return 0;
+        int mxL = maxDepth(root->left);
+        int mxR = maxDepth(root->right);
+        return 1 + max(mxL, mxR);
+    }
+};
