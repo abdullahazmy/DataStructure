212dc63197b083b2e842373efdb08ca5c0319a8f
Abdullah Azmy <125517573+abdullahazmy@users.noreply.github.com>
2024-06-19 07:52:45 +0300

    Update vect.cpp

--- vect.cpp
+++ vect.cpp
@@ -146,6 +146,29 @@ public:
         return idx - 1;
     }
 };
+
+// A Method to make a left rotation k times, with O(N) complexity
+    void rotate(vector<int> &nums, int k)
+    {
+        // Left Rotation
+        int rot = k % (int)nums.size();
+        int arr[nums.size() - k];
+        for (int i = 0; i < k; i++)
+            arr[i] = nums[i];
+        for (int i = rot; i < (int)nums.size(); i++)
+        {
+            nums[i - rot] = nums[i];
+        }
+
+        for (int i = (nums.size() - rot); i < nums.size(); i++)
+        {
+            nums[i] = arr[i - (nums.size() - rot)];
+        }
+
+        for (auto i : nums)
+            cout << i << " ";
+    }
+
 int main()
 {
     int n = 5;
