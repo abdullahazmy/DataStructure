1b224ecaf15c0b45b3860be9a64850b9b14c4551
Abdullah Azmy <125517573+abdullahazmy@users.noreply.github.com>
2024-06-19 08:29:42 +0300

    Update vect.cpp

--- vect.cpp
+++ vect.cpp
@@ -145,6 +145,32 @@ public:
         arr[idx] = x;
         return idx - 1;
     }
+
+    void right_rotate(vector<int> &nums, int k)
+    {
+        // Right Rotation
+        int n = k % nums.size();
+        int arr[n];
+
+        // Saving elements
+        for (int i = 0; i < n; i++)
+            arr[i] = nums[nums.size() - n + i];
+
+        // The Rotation
+        for (int i = nums.size() - 1; i >= n; i--)
+        {
+            nums[i] = nums[i - n];
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            nums[i] = arr[i];
+        }
+
+        for (auto i : nums)
+            cout << i << " ";
+    }
+
 };
 
 // A Method to make a left rotation k times, with O(N) complexity
