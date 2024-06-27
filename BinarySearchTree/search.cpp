4af61b83f55c9122732afee6533e3dacef938969
Abdullah Azmy <125517573+abdullahazmy@users.noreply.github.com>
2024-06-27 06:35:13 +0300

    Update search.cpp

--- BinarySearchTree/search.cpp
+++ BinarySearchTree/search.cpp
@@ -1,13 +1,13 @@
 bool _search(TreeNode *root, int target)
 {
-    if (!root)
+    if (!root) // Base case: If the node is null, target is not found
         return false;
 
-    if (root->val == target)
+    if (root->val == target) // If the current node's value is the target, return true
         return true;
 
-    if (target > root->val)
+    if (target > root->val) // If the target is greater, search in the right subtree
         return _search(root->right, target);
 
-    return _search(root->left, target);
+    return _search(root->left, target); // Otherwise, search in the left subtree
 }
