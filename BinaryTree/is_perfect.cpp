f1458d61d081eb1753ae0f21d8aa8783e3e6bae7
Abdullah Azmy <125517573+abdullahazmy@users.noreply.github.com>
2024-06-30 10:00:24 +0300

    Update is_perfect.cpp

--- BinaryTree/is_perfect.cpp
+++ BinaryTree/is_perfect.cpp
@@ -1,1 +1,14 @@
-// Still working on ..
+bool _is_perfectBT(TreeNode *root, int h) {
+    if (!root)
+        return h == 0;
+    return _is_perfectBT(root->left, h - 1) && _is_perfectBT(root->right, h - 1);
+}
+
+bool is_perfect(TreeNode *root) {
+    if (!root)
+        return true;
+    int h = height(root);
+    return _is_perfectBT(root, h);
+}
+
+// The time complexity here is o(n+n) ---> o(n)
