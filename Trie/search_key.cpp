75896683e7786934f787d8abff58c5319fc1e9b9
Abdullah Azmy <125517573+abdullahazmy@users.noreply.github.com>
2024-07-11 17:18:29 +0300

    Create search_key.cpp

---
+++ Trie/search_key.cpp
@@ -0,0 +1,11 @@
+bool search_key(TrieNode *root, string &s)
+{
+    TrieNode *currentNode = root;
+    for (auto c : s)
+    {
+        if (currentNode->childNode[c - 'a'] == nullptr)
+            return false;
+        currentNode = currentNode->childNode[c - 'a'];
+    }
+    return (currentNode->wordend == true);
+}
