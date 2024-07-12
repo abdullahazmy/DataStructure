e65ad46eed31e029c1df186bbc90ef3e1ed2f1bd
Abdullah Azmy <125517573+abdullahazmy@users.noreply.github.com>
2024-07-12 10:33:45 +0300

    Update kth_maxElement.cpp

--- BinaryHeap/kth_maxElement.cpp
+++ BinaryHeap/kth_maxElement.cpp
@@ -3,6 +3,7 @@ class Solution
 public:
     int findKthLargest(vector<int> &nums, int k)
     {
+        priority_queue<int, vector<int>, greater<int>> q; // Sort in ascending order
         for (int i = 0; i < k; i++)
             q.emplace(nums[i]);
         for (int i = k; i < nums.size(); i++)
